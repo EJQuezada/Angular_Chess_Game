6b502e74923b253d1b7bcce66ff7497358815d4b
Edgar Quezada <edgar.quezada@gmail.com>
2025-06-25 17:10:21 +0200

    create chess-board.component files for ts, html, css

--- src/app/modules/chess-board/chess-board.component.ts
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ChessBoard } from '../../chess-logic/chess-board';
+import { FENChar } from '../../chess-logic/models';
 
 @Component({
   selector: 'app-chess-board',
-  imports: [],
   templateUrl: './chess-board.component.html',
   styleUrl: './chess-board.component.css'
 })
-export class ChessBoard {
-
+export class ChessBoardComponent {
+  private chessBoard = new ChessBoard();
+  public chessBoardView: (FENChar|null)[][] = this.chessBoard.chessBoardView;
+  public get playerColor(); Color{return this.chessBoard.playerColor;};
 }
---
+++ src/app/modules/chess-board/chess-board.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chess-board',
+  imports: [],
+  templateUrl: './chess-board.html',
+  styleUrl: './chess-board.css'
+})
+export class ChessBoard {
+
+}
