f5e585b80f508cd3d7c635deb7ae08065cb5fe88
Edgar Quezada <edgar.quezada@gmail.com>
2025-06-26 12:51:31 +0200

    update component files

--- src/app/modules/chess_board/chess_board.component.ts
+++ src/app/modules/chess_board/chess_board.component.ts
@@ -3,9 +3,9 @@ import { ChessBoard } from '../../chess-logic/chess-board';
 import { Color, FENChar } from '../../chess-logic/models';
 
 @Component({
-  selector: 'app-chess-board',
-  templateUrl: './chess-board.component.html',
-  styleUrl: './chess-board.component.css'
+  selector: 'app-chess_board',
+  templateUrl: './chess_board.component.html',
+  styleUrl: './chess_board.component.css'
 })
 export class ChessBoardComponent {
   private chessBoard = new ChessBoard();
